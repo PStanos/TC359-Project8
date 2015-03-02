8c55f23dd89d38dcbbd8e17ba3acaf108d9a2676
PStanos <paulstanos@gmail.com>
2015-03-02 15:43:24 -0500

    Fixed Bootstrap warning

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -12,7 +12,10 @@ module MethodsPracticeHelpers
     # to remember all HTML needed to do it right). It should show the message in
     # the "message" variable and should have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
-    %(<div class="alert alert-#{type}">#{message}</div>)
+    %(<div class="alert alert-#{type} alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      #{message}
+    </div>)
   end
 
   def current_date_and_time
